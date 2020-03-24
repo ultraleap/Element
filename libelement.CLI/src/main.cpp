92d47e494507d17c436b2213482240a4feae718f
James Munro <james.i.munro@gmail.com>
2020-03-24 12:35:45 +0000

    Added rapidjson and CLI dependencies

--- libelement.CLI/src/main.cpp
+++ libelement.CLI/src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "rapidjson/document.h"
+#include "CLI/CLI.hpp"
+
 int main()
 {
     std::cout << R"({"MessageCode":null,"MessageLevel":null,"Context":"Hello World!","TraceStack":[]})";
