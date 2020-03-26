22c0dce21ac99e9ce712a60cb6d971944ea42875
James Munro <james.i.munro@gmail.com>
2020-03-26 11:16:44 +0000

    Finished moving submodules

--- libelement.CLI/src/main.cpp
+++ libelement.CLI/src/main.cpp
@@ -3,10 +3,17 @@
 #include <CLI/CLI.hpp>
 #include <compiler_message.hpp>
 
-int main()
+int main(int argc, char** argv)
 {
+	CLI::App app{ "CLI interface for libelement" };
+
+	std::string filename = "default";
+	app.add_option("-f,--file", filename, "A help string");
+
+	CLI11_PARSE(app, argc, argv);
+
+
 	cli::compiler_message message(10, cli::message_level::ERROR, "Hello World!", std::vector<cli::trace_site>{});
 
 	std::cout << message.serialize();
-    //std::cout << R"({"MessageCode":null,"MessageLevel":null,"Context":"Hello World!","TraceStack":[]})";
 }
