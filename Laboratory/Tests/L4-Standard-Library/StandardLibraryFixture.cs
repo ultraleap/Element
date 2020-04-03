18e64e151ff5baafa85b44cc86a640dba19ae09f
James Munro <james.i.munro@gmail.com>
2020-04-03 12:31:26 +0100

    Added standard library test stub

---
+++ Laboratory/Tests/L4-Standard-Library/StandardLibraryFixture.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Element;
+
+namespace Laboratory.Tests
+{
+    internal abstract class StandardLibraryFixture : HostFixture
+    {
+        protected CompilationInput ValidatedCompilationInput { get; } = new CompilationInput(FailOnError)
+        {
+            Packages = new[] { new DirectoryInfo("StandardLibrary") },
+        };
+        protected CompilationInput NonValidatedCompilationInput { get; } = new CompilationInput(FailOnError)
+        {
+            Packages = new[] { new DirectoryInfo("StandardLibrary") },
+            SkipValidation = true
+        };
+    }
+}
