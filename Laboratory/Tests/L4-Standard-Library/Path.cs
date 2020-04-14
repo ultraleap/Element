3ce44902661b8e3caf9940011a0fc436904ebb05
James Munro <james.i.munro@gmail.com>
2020-04-14 17:12:34 +0100

    Adjusted ping-pong and repeated line paths

--- Laboratory/Tests/L4-Standard-Library/Path.cs
+++ Laboratory/Tests/L4-Standard-Library/Path.cs
@@ -21,22 +21,36 @@ namespace Laboratory.Tests.L4.StandardLibrary
 
         //TEST
         [
-            TestCase("Path.RepeatingLine(Vector3.Zero, Vector3(10, 20, 30)).at(-0.5)", "Vector3(5, 10, 15)"),
+            TestCase("Path.RepeatingLine(Vector3.Zero, Vector3(10, 20, 30)).at(-0.25)", "Vector3(7.5, 15, 22.5)"),
             TestCase("Path.RepeatingLine(Vector3.Zero, Vector3(10, 20, 30)).at(0)", "Vector3(0, 0, 0)"),
+            TestCase("Path.RepeatingLine(Vector3.Zero, Vector3(10, 20, 30)).at(0.25)", "Vector3(2.5, 5, 7.5)"),
             TestCase("Path.RepeatingLine(Vector3.Zero, Vector3(10, 20, 30)).at(0.5)", "Vector3(5, 10, 15)"),
+            TestCase("Path.RepeatingLine(Vector3.Zero, Vector3(10, 20, 30)).at(0.75)", "Vector3(7.5, 15, 22.5)"),
             TestCase("Path.RepeatingLine(Vector3.Zero, Vector3(10, 20, 30)).at(1)", "Vector3(0, 0, 0)"),
-            TestCase("Path.RepeatingLine(Vector3.Zero, Vector3(10, 20, 30)).at(1.5)", "Vector3(5, 10, 15)"),
+            TestCase("Path.RepeatingLine(Vector3.Zero, Vector3(10, 20, 30)).at(1.25)", "Vector3(2.5, 5, 7.5)"),
         ]
         public void EvaluateRepeatingLinePath(string expression, string expected) =>
             AssertApproxEqual(ValidatedCompilationInput, expected, expression);
             
         //TEST
         [
-            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(-0.5)", "Vector3(-5, -7.5, -15)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(-2)", "Vector3(0, 0, 0)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(-1.75)", "Vector3(2.5, 5, 7.5)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(-1.5)", "Vector3(5, 10, 15)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(-1.25)", "Vector3(7.5, 15, 22.5)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(-1)", "Vector3(10, 20, 30)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(-0.75)", "Vector3(7.5, 15, 22.5)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(-0.5)", "Vector3(5, 10, 15)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(-0.25)", "Vector3(2.5, 5, 7.5)"),
             TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(0)", "Vector3(0, 0, 0)"),
-            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(0.5)", "Vector3(5, 7.5, 15)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(0.25)", "Vector3(2.5, 5, 7.5)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(0.5)", "Vector3(5, 10, 15)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(0.75)", "Vector3(7.5, 15, 22.5)"),
             TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(1)", "Vector3(10, 20, 30)"),
-            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(1.5)", "Vector3(5, 7.5, 15)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(1.25)", "Vector3(7.5, 15, 22.5)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(1.5)", "Vector3(5, 10, 15)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(1.75)", "Vector3(2.5, 5, 7.5)"),
+            TestCase("Path.PingPongLine(Vector3.Zero, Vector3(10, 20, 30)).at(2)", "Vector3(0, 0, 0)"),
         ]
         public void EvaluatePingPongLinePath(string expression, string expected) =>
             AssertApproxEqual(ValidatedCompilationInput, expected, expression);
