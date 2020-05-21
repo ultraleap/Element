49d6eefe5e4b72fa127e283d10faa30927d3e747
James Munro <james.i.munro@gmail.com>
2020-05-21 15:21:08 +0100

    Fix for implicit types on ports

--- libelement/src/common.cpp
+++ libelement/src/common.cpp
@@ -90,8 +90,11 @@ std::string ast_to_string(const element_ast* ast, int depth, const element_ast* 
     else if (ast->type == ELEMENT_AST_NODE_PORT) {
         string += fmt::format("PORT: {}", ast->identifier);
     }
-    else if (ast->type == ELEMENT_AST_NODE_UNSPECIFIED_TYPE) {
-		string += "IMPLICIT RETURN";
+    else if (ast->type == ELEMENT_AST_NODE_UNSPECIFIED_TYPE && ast->parent && ast->parent->type == ELEMENT_AST_NODE_PORT) {
+		string += "IMPLICIT TYPE";
+    }
+    else if (ast->type == ELEMENT_AST_NODE_UNSPECIFIED_TYPE && ast->parent && ast->parent->type == ELEMENT_AST_NODE_DECLARATION) {
+        string += "IMPLICIT RETURN";
     }
     else if (ast->type == ELEMENT_AST_NODE_NONE) {
         if (ast->has_flag(ELEMENT_AST_FLAG_DECL_EMPTY_INPUT))
