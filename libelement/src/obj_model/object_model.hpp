a042492afe03e0d610c645a28ba3c48b4f6e68d1
James Munro <james.i.munro@gmail.com>
2020-06-10 16:21:06 +0100

    First-pass of amendments w.r.t. feedback suggestions from Ricardo

--- libelement/src/obj_model/object_model.hpp
+++ libelement/src/obj_model/object_model.hpp
@@ -7,14 +7,14 @@
 
 namespace element
 {
-	std::unique_ptr<declaration> build_struct_declaration(element_ast* ast, const scope* parent_scope);
-	std::unique_ptr<declaration> build_constraint_declaration(element_ast* ast, const scope* parent_scope);
-	std::unique_ptr<declaration> build_function_declaration(element_ast* ast, const scope* parent_scope);
-	std::unique_ptr<declaration> build_scope_bodied_function_declaration(element_ast* ast, const scope* parent_scope);
-	std::unique_ptr<declaration> build_expression_bodied_function_declaration(element_ast* ast, const scope* parent_scope);
-	std::unique_ptr<declaration> build_namespace_declaration(element_ast* ast, const scope* parent_scope);
-	std::unique_ptr<declaration> build_declaration(element_ast* ast, const scope* parent_scope);
-	std::shared_ptr<expression> build_expression(element_ast* ast, std::shared_ptr<element::expression> parent);
-	std::unique_ptr<root_scope> build_root_scope(element_ast* ast);
+	std::unique_ptr<declaration> build_struct_declaration(const element_ast* ast, const scope* parent_scope);
+	std::unique_ptr<declaration> build_constraint_declaration(const element_ast* ast, const scope* parent_scope);
+	std::unique_ptr<declaration> build_function_declaration(const element_ast* ast, const scope* parent_scope);
+	std::unique_ptr<declaration> build_scope_bodied_function_declaration(const element_ast* ast, const scope* parent_scope);
+	std::unique_ptr<declaration> build_expression_bodied_function_declaration(const element_ast* ast, const scope* parent_scope);
+	std::unique_ptr<declaration> build_namespace_declaration(const element_ast* ast, const scope* parent_scope);
+	std::unique_ptr<declaration> build_declaration(const element_ast* ast, const scope* parent_scope);
+	std::shared_ptr<expression> build_expression(const element_ast* ast, std::shared_ptr<element::expression> parent);
+	std::unique_ptr<root_scope> build_root_scope(const element_ast* ast);
 }
 
