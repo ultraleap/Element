058fed146ad9cf4b724cc249389522f1658bb248
James Munro <james.i.munro@gmail.com>
2020-06-12 16:03:13 +0100

    Little bit of cleanup

--- libelement/src/obj_model/intermediaries/struct_instance.cpp
+++ libelement/src/obj_model/intermediaries/struct_instance.cpp
@@ -1,20 +1,7 @@
 #include "struct_instance.hpp"
 
-element::struct_instance::struct_instance(const element::struct_declaration* declarer, const std::vector<std::shared_ptr<expression>>& expressions)
-    : declarer{ declarer }
-{
-    //TODO: JM - variadics
-    assert(declarer->inputs.size() == expressions.size());
-    for (size_t i = 0; i < declarer->inputs.size(); ++i)
-    {
-        fields.emplace(declarer->inputs[i].identifier, expressions[i]);
-    }
-}
+//compiled_expression
 
-std::string element::struct_instance::to_string() const
-{
-    return "Instance:" + declarer->to_string();
-}
 
 const element::element_object* element::compiled_expression::index(const indexing_expression*) const
 {
@@ -22,5 +9,50 @@ const element::element_object* element::compiled_expression::index(const indexin
     //then we create a function_instance of that function, with ourselves as the first provided argument
     //when we return that function_instance, either the next expression is a call which fills the remaining arguments and then calls it
     //or we just return it/store it, to be used later
+    return nullptr;
+}
+
+
+//struct_instance
+element::struct_instance::struct_instance(const struct_declaration* declarer, const std::vector<std::shared_ptr<expression>>& expressions)
+    : declarer{ declarer }
+{
+    //TODO: JM - variadics
+    assert(declarer->inputs.size() == expressions.size());
+    for (size_t i = 0; i < declarer->inputs.size(); ++i)
+    {
+        //fields.emplace(declarer->inputs[i].identifier, expressions[i]);
+    }
+}
+
+const element::element_object* element::struct_instance::index(const indexing_expression* expression) const
+{
+    //do things
+    return nullptr;
+}
+
+
+std::string element::struct_instance::to_string() const
+{
+    return "Instance:" + declarer->to_string();
+}
+
+//function_instance
+
+
+element::function_instance::function_instance(const function_declaration* declarer, const std::vector<std::shared_ptr<expression>>& expressions)
+    : declarer{declarer}
+{
+    //TODO: JM - variadics
+    assert(declarer->inputs.size() == expressions.size());
+    for (size_t i = 0; i < declarer->inputs.size(); ++i)
+    {
+        //provided_arguments.emplace(declarer->inputs[i].identifier, expressions[i]);
+    }
+}
+
+const element::element_object* element::function_instance::call(const call_expression* expression) const
+{
+    //do things
     return nullptr;
 }
--- libelement/src/obj_model/intermediaries/struct_instance.hpp
+++ libelement/src/obj_model/intermediaries/struct_instance.hpp
@@ -4,39 +4,36 @@
 
 namespace element
 {
-
     //todo: cba to make new headers atm
-    struct compiled_expression : element_object
+    struct compiled_expression final : element_object
     {
-        const element_object* source = nullptr; //literal, struct instance, function instance?
+        const element_object* declarer = nullptr; //literal, struct instance, function instance?
         constraint_const_shared_ptr constraint = element_type::any;
         const expression_shared_ptr expression;
 
         [[nodiscard]] const element_object* index(const indexing_expression*) const override;
+        [[nodiscard]] std::string to_string() const override { return ""; }
     };
 
-    struct struct_instance : element_object
+    struct struct_instance final : element_object
     {
         const struct_declaration* const declarer;
         std::map<std::string, std::unique_ptr<compiled_expression>> fields;
 
-        explicit struct_instance(const element::struct_declaration* declarer, const std::vector<std::shared_ptr<expression>>& expressions);
+        explicit struct_instance(const struct_declaration* declarer, const std::vector<std::shared_ptr<expression>>& expressions);
 
         [[nodiscard]] std::string to_string() const override;
-
         [[nodiscard]] const element_object* index(const indexing_expression*) const override;
     };
 
-    struct function_instance : element_object
+    struct function_instance final : element_object
     {
-        const function_declaration* const source;
+        const function_declaration* const declarer;
         std::map<std::string, std::unique_ptr<compiled_expression>> provided_arguments;
 
-        explicit function_instance(const function_declaration* source, const std::vector<std::shared_ptr<expression>>& expressions);
+        explicit function_instance(const function_declaration* declarer, const std::vector<std::shared_ptr<expression>>& expressions);
 
-        [[nodiscard]] bool is_instance_function() const;
-        [[nodiscard]] std::string to_string() const override;
-
-        [[nodiscard]] const element_object* call(const call_expression*);
+        [[nodiscard]] static bool is_instance_function() { return true; }
+        [[nodiscard]] const element_object* call(const call_expression*) const override;
     };
 }
