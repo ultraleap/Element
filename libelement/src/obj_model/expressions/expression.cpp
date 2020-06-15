ad663ca2429fb899a96b317d7edf877c09cc2961
James Munro <james.i.munro@gmail.com>
2020-06-15 13:58:39 +0100

    Slight cleanup on compile call

--- libelement/src/obj_model/expressions/expression.cpp
+++ libelement/src/obj_model/expressions/expression.cpp
@@ -15,55 +15,10 @@ std::shared_ptr<element::compiled_expression> element::expression::compile() con
         return nullptr; //todo: error_object
 
     //find first thing in the chain
-    std::shared_ptr<element_object> current = children[0];
-
-    //todo: ew dynamic casts. fix and think of a nicer solution to doing a dynamic and then a static
-    if (dynamic_cast<const identifier_expression*>(current.get()))
+    std::shared_ptr<element_object> current = nullptr;
+    for (const auto& expression : children) 
     {
-        const auto* const expr = static_cast<const identifier_expression*>(current.get());
-        current = expr->enclosing_scope->find(expr->identifier, true);
-    }
-    else if (dynamic_cast<const literal_expression*>(current.get()))
-    {
-        const auto* const expr = static_cast<const literal_expression*>(current.get());
-        auto compiled = std::make_shared<compiled_expression>();
-        compiled->expression = std::make_shared<element_expression_constant>(expr->value);
-        //a compiled expression's declarer is always a raw pointer, because it shouldn't be an intermediary but something that's part of the object model. I think
-        //it's basically just some root thing we can use to track down where it came from if we need to
-        compiled->object = current;
-        //todo: don't have constraints set up right now, so just hacking it in to declarer. not even sure if that's a bad thing, declarer makes sense?
-        compiled->declarer = enclosing_scope->get_global()->find("Num", false).get(); //hopefully the Num declaration doesn't move anywhere (e.g merging..)
-        current = std::move(compiled);
-    }
-    else
-    {
-        return nullptr; //todo
-    }
-
-    //do indexing and calling
-    for (std::size_t i = 1; i < children.size(); i++)
-    {
-        auto* const child = children[i].get();
-
-        if (dynamic_cast<const indexing_expression*>(child))
-        {
-            const auto* const expr = static_cast<const indexing_expression*>(child);
-            current = current->index(expr);
-        }
-        else if (dynamic_cast<const call_expression*>(child))
-        {
-            const auto* const expr = static_cast<const call_expression*>(child);
-            std::vector<std::shared_ptr<compiled_expression>> compiled_arguments;
-            for (auto& arg : expr->children)
-            {
-                compiled_arguments.push_back(arg->compile());
-            }
-            current = current->call(std::move(compiled_arguments));
-        }
-        else
-        {
-            return nullptr; //todo:
-        }
+        current = expression->compile(current);
     }
 
     if (dynamic_cast<compiled_expression*>(current.get()))
@@ -71,7 +26,7 @@ std::shared_ptr<element::compiled_expression> element::expression::compile() con
         return std::static_pointer_cast<compiled_expression>(current);
     }
 
-    throw; //todo: could be e.g. a namespace declaration, so not something that was compileable
+    throw; //todo: could be e.g. a namespace declaration, so not something that was compilable
     return nullptr;
 }
 
@@ -96,4 +51,55 @@ std::shared_ptr<element::element_object> element::literal_expression::index(cons
 
     throw;
     return nullptr;
+}
+
+[[nodiscard]] std::shared_ptr<element::element_object> element::identifier_expression::compile(std::shared_ptr<element_object> previous)
+{
+    if (previous) //cannot resolve identifier if previous exists
+        return nullptr;
+
+    if (!enclosing_scope)
+        return nullptr;
+
+    return enclosing_scope->find(identifier, true);
+}
+
+[[nodiscard]] std::shared_ptr<element::element_object> element::literal_expression::compile(std::shared_ptr<element_object> previous)
+{
+    if (previous) //cannot resolve literal if previous exists
+        return nullptr;
+
+    if (!enclosing_scope)
+        return nullptr;
+
+    auto compiled = std::make_shared<compiled_expression>();
+    compiled->expression = std::make_shared<element_expression_constant>(value);
+    //a compiled expression's declarer is always a raw pointer, because it shouldn't be an intermediary but something that's part of the object model. I think
+    //it's basically just some root thing we can use to track down where it came from if we need to
+    compiled->object = shared_from_this();
+    //todo: don't have constraints set up right now, so just hacking it in to declarer. not even sure if that's a bad thing, declarer makes sense?
+    compiled->declarer = enclosing_scope->get_global()->find("Num", false).get(); //hopefully the Num declaration doesn't move anywhere (e.g merging..)
+    return std::move(compiled);
+}
+
+[[nodiscard]] std::shared_ptr<element::element_object> element::indexing_expression::compile(std::shared_ptr<element_object> previous)
+{
+    if (!previous) //can only resolve indexing if previous exists
+        return nullptr;
+
+    return previous->index(this);
+}
+
+[[nodiscard]] std::shared_ptr<element::element_object> element::call_expression::compile(std::shared_ptr<element_object> previous)
+{
+    if (!previous) //can only resolve call if previous exists
+        return nullptr;
+
+    std::vector<std::shared_ptr<compiled_expression>> compiled_arguments;
+    for (const auto& arg : children)
+    {
+        compiled_arguments.push_back(arg->compile());
+    }
+
+    return previous->call(std::move(compiled_arguments));
 }
