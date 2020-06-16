3b9ac75293ed0c8cb6f72c19de827b731af69e2c
Andy Martin <andy.martin@ultraleap.com>
2020-06-16 18:25:17 +0100

    Temporarily switch to CUnit fork to avoid inadvertently bringing in CUnit's tests

--- LMNT/cmake/FetchCUnit.cmake
+++ LMNT/cmake/FetchCUnit.cmake
@@ -1,13 +1,13 @@
 include(FetchContent)
 
 FetchContent_Declare(cunit
-    GIT_REPOSITORY https://gitlab.com/cunity/cunit
-    GIT_TAG        99e89b2ae16594b089a749fc7b9f8383349e3c91 # TODO: 3.2.7
+    GIT_REPOSITORY https://gitlab.com/Esvandiary/cunit  # TODO: https://gitlab.com/cunity/cunit
+    GIT_TAG        allow-disable-tests-and-examples     # TODO: updated commit hash or 3.2.7 when released
     GIT_SHALLOW    true)
 FetchContent_GetProperties(cunit)
 if (NOT cunit_POPULATED)
     FetchContent_Populate(cunit)
-    set(CUNIT_DISABLE_TESTS ON)
-    set(CUNIT_DISABLE_EXAMPLES ON)
+    set(CUNIT_DISABLE_TESTS TRUE)
+    set(CUNIT_DISABLE_EXAMPLES TRUE)
     add_subdirectory("${cunit_SOURCE_DIR}" "${cunit_BINARY_DIR}" EXCLUDE_FROM_ALL)
 endif ()
