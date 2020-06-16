24a50a732107c52f9954a4ec1064c02a571ecf63
Andy Martin <andy.martin@ultraleap.com>
2020-06-16 18:29:37 +0100

    Updated workflow to run CTest in verbose mode, try to fix CUnit build

--- LMNT/cmake/FetchCUnit.cmake
+++ LMNT/cmake/FetchCUnit.cmake
@@ -9,5 +9,12 @@ if (NOT cunit_POPULATED)
     FetchContent_Populate(cunit)
     set(CUNIT_DISABLE_TESTS TRUE)
     set(CUNIT_DISABLE_EXAMPLES TRUE)
+
+    # Modify compile flags to ensure we build CUnit with asserts enabled
+    foreach (n IN ITEMS "" "_RELEASE" "_RELWITHDEBINFO" "_MINSIZEREL" "_DEBUG")
+        string(REGEX REPLACE "[-/]DNDEBUG" "" CMAKE_C_FLAGS${n} "${CMAKE_C_FLAGS${n}}")
+        string(REGEX REPLACE "[-/]DNDEBUG" "" CMAKE_CXX_FLAGS${n} "${CMAKE_CXX_FLAGS${n}}")
+    endforeach ()
+
     add_subdirectory("${cunit_SOURCE_DIR}" "${cunit_BINARY_DIR}" EXCLUDE_FROM_ALL)
 endif ()
