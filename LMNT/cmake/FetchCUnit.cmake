3be36ca0236d03785472557ecaf1df2ee6108a2f
Andy Martin <andy.martin@ultraleap.com>
2020-06-17 18:50:15 +0100

    Moved CUnit back to fixed upstream repo

--- LMNT/cmake/FetchCUnit.cmake
+++ LMNT/cmake/FetchCUnit.cmake
@@ -1,8 +1,8 @@
 include(FetchContent)
 
 FetchContent_Declare(cunit
-    GIT_REPOSITORY https://gitlab.com/Esvandiary/cunit  # TODO: https://gitlab.com/cunity/cunit
-    GIT_TAG        allow-disable-tests-and-examples     # TODO: updated commit hash or 3.2.7 when released
+    GIT_REPOSITORY https://gitlab.com/cunity/cunit
+    GIT_TAG        eccf9ef56f5d8d895ccd4129b80c92f448a6023f
     GIT_SHALLOW    true)
 FetchContent_GetProperties(cunit)
 if (NOT cunit_POPULATED)
