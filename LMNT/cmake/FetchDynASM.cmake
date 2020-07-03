b3b92d66728d47f5ec5e31f6cfd0739091cba370
Andy Martin <andy.martin@ultraleap.com>
2020-07-03 20:10:08 +0100

    Update dynasm to fix buggy codegen for MOVAPS/MOVUPS xmm, m128

--- LMNT/cmake/FetchDynASM.cmake
+++ LMNT/cmake/FetchDynASM.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(dynasm
     GIT_REPOSITORY https://github.com/Esvandiary/dynasm
-    GIT_TAG        63b76d3390f74fb6407e348d8f8fcf9b475a2665
+    GIT_TAG        f0158e129401b934cf49c8017426fd2bf99799c0
     GIT_SHALLOW    true)
 FetchContent_GetProperties(dynasm)
 if (NOT dynasm_POPULATED)
