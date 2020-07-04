f000a301310f7d24e1e2a2a4c935dc3c7ac18615
Andy Martin <andy.martin@ultraleap.com>
2020-07-04 16:35:23 +0100

    LMNT: added tests for trig functions

--- LMNT/test/test_interpreter.c
+++ LMNT/test/test_interpreter.c
@@ -3,6 +3,7 @@
 #include "test_maths_scalar.h"
 #include "test_maths_vector.h"
 #include "test_util.h"
+#include "test_trig.h"
 
 
 int main(int argc, char** argv)
@@ -10,6 +11,7 @@ int main(int argc, char** argv)
     register_suite_maths_scalar();
     register_suite_maths_vector();
     register_suite_util();
+    register_suite_trig();
 
     return CU_CI_main(argc, argv);
 }
