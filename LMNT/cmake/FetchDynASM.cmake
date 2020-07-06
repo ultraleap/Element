f6aebbf08c21d0316b25264f126b5ea11251a9f1
Andy Martin <andy.martin@ultraleap.com>
2020-07-07 00:54:28 +0100

    Updated DynASM to fix several issues with ARMv7-M codegen

--- LMNT/cmake/FetchDynASM.cmake
+++ LMNT/cmake/FetchDynASM.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(dynasm
     GIT_REPOSITORY https://github.com/Esvandiary/dynasm
-    GIT_TAG        f0158e129401b934cf49c8017426fd2bf99799c0
+    GIT_TAG        a51589334fe9edde22e1cd4020bece5b6459a5fb
     GIT_SHALLOW    true)
 FetchContent_GetProperties(dynasm)
 if (NOT dynasm_POPULATED)
