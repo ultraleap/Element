c2958d50bd854023077e02cc208b3faf4dd20edf
Craig Jeffrey <CraigJeffrey3@gmail.com>
2020-07-24 17:25:04 +0100

    Remove tuple since Anonymous block replaces it

--- Element.NET/!2-ValueObjectModel/Intrinsics/Functions/Fold.cs
+++ Element.NET/!2-ValueObjectModel/Intrinsics/Functions/Fold.cs
@@ -20,34 +20,38 @@ namespace Element.AST
 			
 			// TODO: Simplify dynamic fold to a loop expression
 			// Source implementation for a dynamic fold
-			// _(list:List, initial, aggregator:Binary) = for(Tuple(0, initial),
-			//			_(tup):Bool = tup.varg0.lt(count),
-			//			_(tup) = Tuple(tup.varg0.add(1), aggregator(tup.varg1, list.at(tup.varg0)))
+			// _(list:List, initial, aggregator:Binary) = for({i = 0; value = initial;};,
+			//			_(tup):Bool = tup.i.lt(count),
+			//			_(tup) = {i = tup.i.add(1); value = aggregator(tup.value, list.at(tup.i); };)
 
-			static SourceInfo ConditionSourceCode() => new SourceInfo("<compiler generated dynamic fold condition>", "_(tup):Bool = tup.varg0.lt(list.count)");
-			static SourceInfo BodySourceCode() => new SourceInfo("<compiler generated dynamic fold body", "_(tup) = Tuple(tup.varg0.add(1), aggregator(tup.varg1, list.at(tup.varg0)))");
+			static SourceInfo ConditionSourceCode() =>new SourceInfo("<compiler generated dynamic fold condition>",
+			                                                         "_(tup):Bool = tup.i.lt(list.count)");
+			static SourceInfo BodySourceCode() => new SourceInfo("<compiler generated dynamic fold body",
+			                                                     "_(tup) = {i = tup.i.add(1); value = aggregator(tup.value, list.at(tup.i); };)");
 			Result<IValue[]> CreateDynamicFoldArguments() =>
-				context.SourceContext.GlobalScope.Lookup(TupleStruct.Instance.Identifier, context)
-				       .Cast<Struct>(context)
-				       .Bind(tupleDecl => TupleStruct.Instance.Construct(tupleDecl, new[] {Constant.Zero, workingValue}, context)
-				                                   .Accumulate(() => Parser.Parse<Lambda>(ConditionSourceCode(), context),
-				                                               () => Parser.Parse<Lambda>(BodySourceCode(), context))
-				                                   .Bind(t =>
-				                                   {
-					                                   var (initial, predicateLambda, bodyLambda) = t;
-					                                   var captureBlock = new ResolvedBlock(new (Identifier, IValue)[]
-					                                   {
-						                                   (new Identifier("list"), list),
-						                                   (new Identifier("aggregator"), aggregator)
-					                                   }, null);
-					                                   return predicateLambda.ResolveExpression(captureBlock, context)
-					                                                         .Accumulate(() => bodyLambda.ResolveExpression(captureBlock, context))
-					                                                         .Map(r =>
-					                                                         {
-						                                                         var (predicateFunction, bodyFunction) = r;
-						                                                         return new[] {initial, predicateFunction, bodyFunction};
-					                                                         });
-				                                   }));
+				Parser.Parse<Lambda>(ConditionSourceCode(), context)
+				      .Accumulate(() => Parser.Parse<Lambda>(BodySourceCode(), context))
+				      .Bind(t =>
+				      {
+					      var (predicateLambda, bodyLambda) = t;
+					      var initial = new ResolvedBlock(new (Identifier, IValue)[]
+					      {
+						      (new Identifier(""), Constant.Zero),
+						      (new Identifier(""), workingValue)
+					      }, null);
+					      var captureBlock = new ResolvedBlock(new (Identifier, IValue)[]
+					      {
+						      (new Identifier("list"), list),
+						      (new Identifier("aggregator"), aggregator)
+					      }, null);
+					      return predicateLambda.ResolveExpression(captureBlock, context)
+					                            .Accumulate(() => bodyLambda.ResolveExpression(captureBlock, context))
+					                            .Map(r =>
+					                            {
+						                            var (predicateFunction, bodyFunction) = r;
+						                            return new[] {initial, predicateFunction, bodyFunction};
+					                            });
+				      });
 
 			return ListStruct.EvaluateElements(list, context)
 			                 .FoldArray(workingValue, (current, e) => aggregator.Call(new[] {current, e}, context))
