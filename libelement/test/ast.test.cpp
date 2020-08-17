bf2c325c9f682bfa148a833dd7832923219c96d4
Harry Mills <harry.mills@ultraleap.com>
2020-08-17 10:30:07 +0100

    Adding more AST tests

--- libelement/test/ast.test.cpp
+++ libelement/test/ast.test.cpp
@@ -3,7 +3,6 @@
 
 #include "element/ast.h"
 #include "element/token.h"
-
 #include "../src/ast/ast_internal.hpp"
 
 TEST_CASE("Ast Generation", "[AST]") {
@@ -15,6 +14,7 @@ TEST_CASE("Ast Generation", "[AST]") {
 
     SECTION("Example Code - Simple Variable") {
         element_tokeniser_run(tokeniser, "Burger1 = 2;", "<input>");
+
         parser.tokeniser = tokeniser;
         parser.ast_build();
 
@@ -41,6 +41,130 @@ TEST_CASE("Ast Generation", "[AST]") {
         // Literal
         REQUIRE(root->children[0]->children[1]->type == ELEMENT_AST_NODE_LITERAL);
         REQUIRE(root->children[0]->children[1]->literal == 2);
+    }
+
+    SECTION("Example Code - Struct") {
+        element_tokeniser_run(tokeniser,"struct MyStruct(a, b);" ,"<input>");
+
+        parser.tokeniser = tokeniser;
+        parser.ast_build();
+
+        // Root
+        REQUIRE(parser.root->type == ELEMENT_AST_NODE_ROOT);
+
+        auto root = parser.root;
+
+        // Struct
+        REQUIRE(root->children[0]->type == ELEMENT_AST_NODE_STRUCT);
+
+        // Declaration
+        REQUIRE(root->children[0]->children[0]->type == ELEMENT_AST_NODE_DECLARATION);
+        REQUIRE(root->children[0]->children[0]->identifier == "MyStruct");
+
+        // Portlist
+        REQUIRE(root->children[0]->children[0]->children[0]->type == ELEMENT_AST_NODE_PORTLIST);
+        REQUIRE(root->children[0]->children[0]->children[0]->children[0]->type == ELEMENT_AST_NODE_PORT);
+        REQUIRE(root->children[0]->children[0]->children[0]->children[0]->identifier == "a");
+        REQUIRE(root->children[0]->children[0]->children[0]->children[1]->type == ELEMENT_AST_NODE_PORT);
+        REQUIRE(root->children[0]->children[0]->children[0]->children[1]->identifier == "b");
+
+        // Implicit Return
+        REQUIRE(root->children[0]->children[0]->children[1]->type == ELEMENT_AST_NODE_UNSPECIFIED_TYPE);
+        REQUIRE(root->children[0]->children[0]->children[1]->parent->type == ELEMENT_AST_NODE_DECLARATION);
+
+        // No body
+        REQUIRE(root->children[0]->children[1]->type == ELEMENT_AST_NODE_NO_BODY);
+    }
+
+    SECTION("Example Code - Lambda") {
+        element_tokeniser_run(tokeniser, "a = _(_a) = 5;" ,"<input>");
+
+        parser.tokeniser = tokeniser;
+        parser.ast_build();
+
+        // Root
+        REQUIRE(parser.root->type == ELEMENT_AST_NODE_ROOT);
+
+        auto root = parser.root;
+
+        // Function
+        REQUIRE(root->children[0]->type == ELEMENT_AST_NODE_FUNCTION);
+
+        // Declaration
+        REQUIRE(root->children[0]->children[0]->type == ELEMENT_AST_NODE_DECLARATION);
+        REQUIRE(root->children[0]->children[0]->identifier == "a");
+
+        // Empty Inputs
+        REQUIRE(root->children[0]->children[0]->children[0]->type == ELEMENT_AST_NODE_NONE);
+        REQUIRE(root->children[0]->children[0]->children[0]->has_flag(ELEMENT_AST_FLAG_DECL_EMPTY_INPUT));
+
+        // Implicit Return
+        REQUIRE(root->children[0]->children[0]->children[1]->type == ELEMENT_AST_NODE_UNSPECIFIED_TYPE);
+        REQUIRE(root->children[0]->children[0]->children[1]->parent->type == ELEMENT_AST_NODE_DECLARATION);
+
+        // Lambda
+        REQUIRE(root->children[0]->children[1]->type == ELEMENT_AST_NODE_LAMBDA);
+
+        // Portlist
+        REQUIRE(root->children[0]->children[1]->children[0]->type == ELEMENT_AST_NODE_PORTLIST);
+        REQUIRE(root->children[0]->children[1]->children[0]->children[0]->type == ELEMENT_AST_NODE_PORT);
+        REQUIRE(root->children[0]->children[1]->children[0]->children[0]->identifier == "_a");
+
+        // Literal
+        REQUIRE(root->children[0]->children[1]->children[1]->type == ELEMENT_AST_NODE_LITERAL);
+        REQUIRE(root->children[0]->children[1]->children[1]->literal == 5);
+    }
+
+    SECTION("Example Code - Namespace") {
+        element_tokeniser_run(tokeniser, "namespace Empty {}" ,"<input>");
+
+        parser.tokeniser = tokeniser;
+        parser.ast_build();
+
+        // Root
+        REQUIRE(parser.root->type == ELEMENT_AST_NODE_ROOT);
+
+        auto root = parser.root;
+
+        // Namespace
+        REQUIRE(root->children[0]->type == ELEMENT_AST_NODE_NAMESPACE);
+        REQUIRE(root->children[0]->identifier == "Empty");
+
+        // Scope
+        REQUIRE(root->children[0]->children[0]->type == ELEMENT_AST_NODE_SCOPE);
+
+    }
+
+    SECTION("Example Code - Intrinsic Constraint") {
+        element_tokeniser_run(tokeniser, "intrinsic constraint Any;" ,"<input>");
+
+        parser.tokeniser = tokeniser;
+        parser.ast_build();
+
+        // Root
+        REQUIRE(parser.root->type == ELEMENT_AST_NODE_ROOT);
+
+        auto root = parser.root;
+
+        // Constraint
+        REQUIRE(root->children[0]->type == ELEMENT_AST_NODE_CONSTRAINT);
+
+        // Intrinsic Declaration
+        REQUIRE(root->children[0]->children[0]->type == ELEMENT_AST_NODE_DECLARATION);
+        REQUIRE(root->children[0]->children[0]->has_flag(ELEMENT_AST_FLAG_DECL_INTRINSIC));
+        REQUIRE(root->children[0]->children[0]->identifier == "Any");
+
+        // Empty Inputs
+        REQUIRE(root->children[0]->children[0]->children[0]->type == ELEMENT_AST_NODE_NONE);
+        REQUIRE(root->children[0]->children[0]->children[0]->has_flag(ELEMENT_AST_FLAG_DECL_EMPTY_INPUT));
+
+        // Implicit Return
+        REQUIRE(root->children[0]->children[0]->children[1]->type == ELEMENT_AST_NODE_UNSPECIFIED_TYPE);
+        REQUIRE(root->children[0]->children[0]->children[1]->parent->type == ELEMENT_AST_NODE_DECLARATION);
+
+        // No body
+        REQUIRE(root->children[0]->children[1]->type == ELEMENT_AST_NODE_NO_BODY);
+
     }
 
     SECTION("Example Code - Function Call With Inputs") {
@@ -81,7 +205,9 @@ TEST_CASE("Ast Generation", "[AST]") {
         REQUIRE(root->children[0]->children[1]->children[1]->type == ELEMENT_AST_NODE_EXPRLIST);
 
         // Literals
+        REQUIRE(root->children[0]->children[1]->children[1]->children[0]->type == ELEMENT_AST_NODE_LITERAL);
         REQUIRE(root->children[0]->children[1]->children[1]->children[0]->literal == 1);
+        REQUIRE(root->children[0]->children[1]->children[1]->children[1]->type == ELEMENT_AST_NODE_LITERAL);
         REQUIRE(root->children[0]->children[1]->children[1]->children[1]->literal == 2);
     }
 
