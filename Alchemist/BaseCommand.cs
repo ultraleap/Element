37e51c65845be0828992a62c8a82382778c0e74e
Craig Jeffrey <CraigJeffrey3@gmail.com>
2020-09-16 21:54:13 +0100

    Alchemist compiles again

--- Alchemist/BaseCommand.cs
+++ Alchemist/BaseCommand.cs
@@ -57,7 +57,7 @@ namespace Alchemist
 				}
 				catch (Exception e)
 				{
-					Log(e.ToString(), MessageLevel.Error);
+					Log(e.ToString(), level: MessageLevel.Error);
 				}
 			}
 
@@ -71,7 +71,7 @@ namespace Alchemist
 				}
 				catch (Exception e)
 				{
-					Log(e.ToString(), MessageLevel.Error);
+					Log(e.ToString(), level: MessageLevel.Error);
 				}
 			}
 
@@ -92,7 +92,7 @@ namespace Alchemist
 			return result.IsSuccess ? 0 : 1;
 		}
 
-		private void Log(string message, MessageLevel? level = default) => Log(new CompilerMessage(message, level));
+		private void Log(string message, string messageType = "", MessageLevel? level = default) => Log(new CompilerMessage(message, messageType, level));
 
 		private void Log(CompilerMessage message)
 		{
