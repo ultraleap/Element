6ad2038aa586c3b6c63e6a03dc92b487414b436d
Greg Oliver <greg.oliver@ultraleap.com>
2020-10-27 18:19:29 +0000

    Laboratory: Duplicate UnitTests
    Refactor the unit tests to test functions in both
    compiled and interpretted mode.

--- Laboratory/Tests/L4-Standard-Library/ElementTimeSpan.cs
+++ Laboratory/Tests/L4-Standard-Library/ElementTimeSpan.cs
@@ -6,100 +6,136 @@ namespace Laboratory.Tests.L4.StandardLibrary
 {
     internal class ElementTimeSpan : StandardLibraryFixture
     {
-
         // Most tests are written for the "fromSeconds" factory function, as the default constructor hides the implementation
         // detail of separating out the fractional and integer components.
-        [
-            TestCase("TimeSpan.fromSeconds(1).add(TimeSpan.fromSeconds(2))", "TimeSpan.fromSeconds(3)"),  // Integer seconds can be added 
-            TestCase("TimeSpan.fromSeconds(1).add(TimeSpan.fromSeconds(4))", "TimeSpan.fromSeconds(5)"),
-            TestCase("TimeSpan.fromSeconds(0.2).add(TimeSpan.fromSeconds(0.3))", "TimeSpan.fromSeconds(0.5)"),  // Fractional parts can be added 
-            TestCase("TimeSpan.fromSeconds(0.7).add(TimeSpan.fromSeconds(0.8))", "TimeSpan.fromSeconds(1.5)"),
-            TestCase("TimeSpan.fromSeconds(3.5).add(TimeSpan.fromSeconds(4.8))", "TimeSpan.fromSeconds(8.3)"),  // Complete numbers can be added
-        ]
-        public void AddPositive(string expression, string expected) =>
-            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+        public static string TimeSpanFromSeconds = "TimeSpan.fromSeconds";
 
-        [
-            TestCase("TimeSpan.fromSeconds(-1).add(TimeSpan.fromSeconds(-3))", "TimeSpan.fromSeconds(-4)"),    // The integer parts can be added
-            TestCase("TimeSpan.fromSeconds(-1.3).add(TimeSpan.fromSeconds(-1.2))", "TimeSpan.fromSeconds(-2.5)"),    // The fractional parts can be added
-        ]
-        public void AddNegative(string expression, string expected) =>
-            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+        public static string TimeSpanAdder = "_(a:Num, b:Num):TimeSpan = TimeSpan.fromSeconds(a).add(TimeSpan.fromSeconds(b))";
+        public static (string LeftHandArgs, string RightHandArgs)[] AddArgsList =
+        {
+            ("(1, 2)", "(3)"), // Integer seconds can be added
+            ("(1, 4)", "(5)"),
+            ("(0.2, 0.3)", "(0.5)"), // Fractional parts can be added
+            ("(0.7, 0.8)", "(1.5)"),
+            ("(3.5, 4.8)", "(8.3)"), // Complete numbers can be added
+            ("(-1, -3)", "(-4)"),    // The integer parts can be added
+            ("(-1.3, -1.2)", "(-2.5)"), // The fractional parts can be added
+        };
 
-
-
-        [
-            TestCase("TimeSpan.fromSeconds(10).sub(TimeSpan.fromSeconds(3.4))", "TimeSpan.fromSeconds(6.6)"),
-            TestCase("TimeSpan.fromSeconds(10).sub(TimeSpan.fromSeconds(3.6))", "TimeSpan.fromSeconds(6.4)"),
-            TestCase("TimeSpan.fromSeconds(1).sub(TimeSpan.fromSeconds(0.4))", "TimeSpan.fromSeconds(0.6)"),
-            TestCase("TimeSpan.fromSeconds(1).sub(TimeSpan.fromSeconds(0.5))", "TimeSpan.fromSeconds(0.5)"),
-            TestCase("TimeSpan.fromSeconds(1).sub(TimeSpan.fromSeconds(0.6))", "TimeSpan.fromSeconds(0.4)"),
-            TestCase("TimeSpan.fromSeconds(1).sub(TimeSpan.fromSeconds(1))", "TimeSpan.fromSeconds(0)"),
-            TestCase("TimeSpan.fromSeconds(1).sub(TimeSpan.fromSeconds(1.4))", "TimeSpan.fromSeconds(-0.4)"),
-            TestCase("TimeSpan.fromSeconds(1).sub(TimeSpan.fromSeconds(1.6))", "TimeSpan.fromSeconds(-0.6)"),
-            TestCase("TimeSpan.fromSeconds(1).sub(TimeSpan.fromSeconds(2.5))", "TimeSpan.fromSeconds(-1.5)"),
-        ]
-        public void Subtract(string expression, string expected) =>
-            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+        [Test]
+        public void AddTimeSpans([ValueSource(nameof(AddArgsList))] (string lhs, string rhs) args, [Values] EvaluationMode mode)
+        {
+            AssertApproxEqual(ValidatedCompilerInput,
+                              new FunctionEvaluation(TimeSpanAdder, args.lhs, mode),
+                              new FunctionEvaluation(TimeSpanFromSeconds, args.rhs, mode));
+        }
 
 
-        [
-            TestCase("TimeSpan.fromSeconds(2.3).mul(2)", "TimeSpan.fromSeconds(4.6)"),  // Numbers can be multiplied
-            TestCase("TimeSpan.fromSeconds(2.7).mul(2)", "TimeSpan.fromSeconds(5.4)"),      // Fractional parts cannot exceed 1
-            TestCase("TimeSpan.fromSeconds(2.72).mul(10)", "TimeSpan.fromSeconds(27.2)"),
-        ]
-        public void MulPositive(string expression, string expected) =>
-            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+        public static string TimeSpanSubtractor = "_(a:Num, b:Num):TimeSpan = TimeSpan.fromSeconds(a).sub(TimeSpan.fromSeconds(b))";
+        public static (string LeftHandArgs, string RightHandArgs)[] SubArgsList =
+        {
+            ("10, 3.4", "6.6"),
+            ("10, 3.6", "6.4"),
+            ("1, 0.4", "0.6"),
+            ("1, 0.5", "0.5"),
+            ("1, 0.6", "0.4"),
+            ("1, 1", "0"),
+            ("1, 1.4", "-0.4"),
+            ("1, 1.6", "-0.5"),
+            ("1, 2.5", "-1.5"),
+        };
 
-        [
-            TestCase("TimeSpan.fromSeconds(1.7).mul(-1)", "TimeSpan.fromSeconds(-1.7)"),    // Numbers can be multiplied by negative numbers
-            TestCase("TimeSpan.fromSeconds(2.7).mul(-2)", "TimeSpan.fromSeconds(-5.4)"),
-            TestCase("TimeSpan.fromSeconds(2.72).mul(-10)", "TimeSpan.fromSeconds(-27.2)"),
-            TestCase("TimeSpan.fromSeconds(-2.3).mul(-2)", "TimeSpan.fromSeconds(4.6)"),
-            TestCase("TimeSpan.fromSeconds(-2.7).mul(-2)", "TimeSpan.fromSeconds(5.4)"),
-            TestCase("TimeSpan.fromSeconds(-2.72).mul(-10)", "TimeSpan.fromSeconds(27.2)"),
-
-        ]
-        public void MulNegative(string expression, string expected) =>
-            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+        [Test]
+        public void SubtractTimeSpans([ValueSource(nameof(SubArgsList))] (string lhs, string rhs) args, [Values] EvaluationMode mode)
+        {
+            AssertApproxEqual(ValidatedCompilerInput,
+                              new FunctionEvaluation(TimeSpanSubtractor, args.lhs, mode),
+                              new FunctionEvaluation(TimeSpanFromSeconds, args.rhs, mode));
+        }
 
 
-        [
-            TestCase("TimeSpan.fromSeconds(2.3).div(2)", "TimeSpan.fromSeconds(1.15)"),  // Numbers can be multiplied
-            TestCase("TimeSpan.fromSeconds(2.7).div(2)", "TimeSpan.fromSeconds(1.35)"),      // Fractional parts cannot exceed 1
-            TestCase("TimeSpan.fromSeconds(2.72).div(10)", "TimeSpan.fromSeconds(0.272)"),
-            TestCase("TimeSpan.fromSeconds(1.7).div(-1)", "TimeSpan.fromSeconds(-1.7)"),    // Numbers can be multiplied by negative numbers
-            TestCase("TimeSpan.fromSeconds(2.7).div(-2)", "TimeSpan.fromSeconds(-1.35)"),
-            TestCase("TimeSpan.fromSeconds(2.72).div(-10)", "TimeSpan.fromSeconds(-0.272)"),
-        ]
-        public void Div(string expression, string expected) =>
-            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+        public static string TimeSpanMultiplier = "_(a:Num, b:Num):TimeSpan = TimeSpan.fromSeconds(a).mul(b)";
+        public static (string LeftHandArgs, string RightHandArgs)[] MulArgsList =
+        {
+            ("(2.3, 2)", "(4.6)"),
+            ("(2.7, 2)", "(5.4)"),
+            ("(2.72, 10)", "(27.2)"),
+            ("(1.7, -1)", "(-1.7)"),
+            ("(2.7, -2)", "(-5.4)"),
+            ("(2.72, -10)", "(-27.2)"),
+            ("(-2.3, -2)", "(4.6)"),
+            ("(-2.7, -2)", "(5.4)"),
+            ("(-2.72, -10)", "(27.2)"),
+        };
 
-        [
-            TestCase("TimeSpan.fromSeconds(1).abs", "TimeSpan.fromSeconds(1)"),
-            TestCase("TimeSpan.fromSeconds(1.5).abs", "TimeSpan.fromSeconds(1.5)"),
-            TestCase("TimeSpan.fromSeconds(1.75).abs", "TimeSpan.fromSeconds(1.75)"),
-            TestCase("TimeSpan.fromSeconds(2).abs", "TimeSpan.fromSeconds(2)"),
-            TestCase("TimeSpan.fromSeconds(-1.5).abs", "TimeSpan.fromSeconds(1.5).abs"),
-            TestCase("TimeSpan.fromSeconds(-1.75).abs", "TimeSpan.fromSeconds(1.75).abs"),
-            TestCase("TimeSpan.fromSeconds(-2).abs", "TimeSpan.fromSeconds(2).abs"),
-        ]
-        public void Abs(string expression, string expected) =>
-            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+        [Test]
+        public void MultiplyTimeSpanByNumber([ValueSource(nameof(MulArgsList))] (string lhs, string rhs) args, [Values] EvaluationMode mode)
+        {
+            AssertApproxEqual(ValidatedCompilerInput,
+                              new FunctionEvaluation(TimeSpanMultiplier, args.lhs, mode),
+                              new FunctionEvaluation(TimeSpanFromSeconds, args.rhs, mode));
+        }
 
-        [
-            TestCase("TimeSpan.fromSeconds(1).negate", "TimeSpan.fromSeconds(-1)"),
-            TestCase("TimeSpan.fromSeconds(1.5).negate", "TimeSpan.fromSeconds(-1.5)"),
-            TestCase("TimeSpan.fromSeconds(1.75).negate", "TimeSpan.fromSeconds(-1.75)"),
-            TestCase("TimeSpan.fromSeconds(2).negate", "TimeSpan.fromSeconds(-2)"),
-            TestCase("TimeSpan.fromSeconds(-1).negate", "TimeSpan.fromSeconds(1)"),
-            TestCase("TimeSpan.fromSeconds(-1.5).negate", "TimeSpan.fromSeconds(1.5)"),
-            TestCase("TimeSpan.fromSeconds(-1.75).negate", "TimeSpan.fromSeconds(1.75)"),
-            TestCase("TimeSpan.fromSeconds(-2).negate", "TimeSpan.fromSeconds(2)"),
-        ]
-        public void Negate(string expression, string expected) =>
-            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
 
+        public static string TimeSpanDivider = "_(a:Num, b:Num):TimeSpan = TimeSpan.fromSeconds(a).div(b)";
+        public static (string LeftHandArgs, string RightHandArgs)[] DivArgsList =
+        {
+            ("(2.3, 2)", "(1.15)"),
+            ("(2.7, 2)", "(1.35)"),
+            ("(2.72, 10)", "(0.272)"),
+            ("(1.7, -1)", "(-1.7)"),
+            ("(2.7, -2)", "(-1.35)"),
+            ("(2.72, -10)", "(-0.272)"),
+        };
+
+        [Test]
+        public void DivideTimeSpanByNumber([ValueSource(nameof(DivArgsList))] (string lhs, string rhs) args, [Values] EvaluationMode mode)
+        {
+            AssertApproxEqual(ValidatedCompilerInput,
+                              new FunctionEvaluation(TimeSpanDivider, args.lhs, mode),
+                              new FunctionEvaluation(TimeSpanFromSeconds, args.rhs, mode));
+        }
+
+        public static string TimeSpanAbsoluter = "_(a:Num):TimeSpan = TimeSpan.fromSeconds(a).abs";
+        public static (string LeftHandArgs, string RightHandArgs)[] AbsArgsList =
+        {
+            ("(1)", "(1)"),
+            ("(1.5)", "(1.5)"),
+            ("(1.75)", "(1.75)"),
+            ("(2)", "(2)"),
+            ("(-1.5)", "(1.5)"),
+            ("(-1.75)", "(1.75)"),
+            ("(-2)", "(2)"),
+        };
+
+        [Test]
+        public void AbsoluteTimeSpan([ValueSource(nameof(AbsArgsList))] (string lhs, string rhs) args, [Values] EvaluationMode mode)
+        {
+            AssertApproxEqual(ValidatedCompilerInput,
+                              new FunctionEvaluation(TimeSpanAbsoluter, args.lhs, mode),
+                              new FunctionEvaluation(TimeSpanFromSeconds, args.rhs, mode));
+        }
+
+        public static string TimeSpanNegator = "_(a:Num):TimeSpan = TimeSpan.fromSeconds(a).negate";
+        public static (string LeftHandArgs, string RightHandArgs)[] NegatorArgsList =
+        {
+            ("(1)", "(-1)"),
+            ("(1.5)", "(-1.5)"),
+            ("(1.75)", "(-1.75)"),
+            ("(2)", "(-2)"),
+            ("(-1)", "(1)"),
+            ("(-1.5)", "(1.5)"),
+            ("(-1.75)", "(1.75)"),
+            ("(-2)", "(2)"),
+        };
+
+        [Test]
+        public void NegateTimeSpan([ValueSource(nameof(NegatorArgsList))] (string lhs, string rhs) args, [Values] EvaluationMode mode)
+        {
+            AssertApproxEqual(ValidatedCompilerInput,
+                              new FunctionEvaluation(TimeSpanNegator, args.lhs, mode),
+                              new FunctionEvaluation(TimeSpanFromSeconds, args.rhs, mode));
+        }
 
         private static (int FrameIndex, int SampleRate, float Period)[] _args =
             new[]
