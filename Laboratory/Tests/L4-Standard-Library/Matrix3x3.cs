4a206498bbdbe03d63faa5cc6cdc10452554ea48
Greg Oliver <greg.oliver@ultraleap.com>
2020-10-28 18:26:15 +0000

    Element: Update Matrix3x3

---
+++ Laboratory/Tests/L4-Standard-Library/Matrix3x3.cs
@@ -0,0 +1,116 @@
+﻿using NUnit.Framework;
+
+namespace Laboratory.Tests.L4.StandardLibrary
+{
+    internal class Matrix3x3 : StandardLibraryFixture
+    {
+        [
+            TestCase(
+            "Matrix3x3.identity",
+            "Matrix3x3(" +
+                "Vector3(1, 0, 0), " +
+                "Vector3(0, 1, 0), " +
+                "Vector3(0, 0, 1)" +
+                ")"
+            ),
+
+        ]
+        public void CheckIdentity(string expression, string expected) =>
+            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+        
+        [
+            TestCase(
+            "Matrix3x3(" +
+                "Vector3(1, 4, 7), " +
+                "Vector3(2, 5, 8), " +
+                "Vector3(3, 6, 9)" +
+                ").xRow",
+            "Vector3(1, 2, 3)"
+            ),
+            TestCase(
+            "Matrix3x3(" +
+                "Vector3(1, 4, 7), " +
+                "Vector3(2, 5, 8), " +
+                "Vector3(3, 6, 9)" +
+                ").yRow",
+            "Vector3(4, 5, 6)"
+            ),
+            TestCase(
+            "Matrix3x3(" +
+                "Vector3(1, 4, 7), " +
+                "Vector3(2, 5, 8), " +
+                "Vector3(3, 6, 9)" +
+                ").zRow",
+            "Vector3(7, 8, 9)"
+            ),
+            TestCase(
+            "Matrix3x3(" +
+                "Vector3(1, 4, 7), " +
+                "Vector3(2, 5, 8), " +
+                "Vector3(3, 6, 9)" +
+                ").diagonal",
+            "Vector3(1, 5, 9)"
+            ),
+        ]
+        public void CheckVectors(string expression, string expected) =>
+            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+
+
+        [
+            TestCase(
+            "Matrix3x3.identity.determinant",
+            "1"
+            ),
+            TestCase(
+            "Matrix3x3(" +
+                "Vector3(1, 4, 7), " +
+                "Vector3(2, 5, 8), " +
+                "Vector3(3, 6, 9)" +
+                ").determinant",
+            "0"
+            ),
+            TestCase(
+            "Matrix3x3(" +
+                "Vector3(2, 0, 0), " +
+                "Vector3(0, 2, 0), " +
+                "Vector3(0, 0, 2)" +
+                ").determinant",
+            "8"
+            ),
+        ]
+        public void CheckDeterminant(string expression, string expected) =>
+            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+
+
+        [
+            TestCase(
+            "Matrix3x3.identity" +
+            ".mulVec(Vector3(1, 2, 3))",
+            "Vector3(1, 2, 3)"
+            ),
+            TestCase(
+            "Matrix3x3(Vector3(1, 4, 7), Vector3(2, 5, 8), Vector3(3, 6, 9))" +
+            ".mulVec(Vector3(1, 2, 3))",
+            "Vector3(14, 32, 50)"
+            ),
+        ]
+        public void CheckMulVec(string expression, string expected) =>
+            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+
+        [
+            TestCase(
+            "Matrix3x3.identity" +
+            ".mul(Matrix3x3(Vector3(1, 4, 7), Vector3(2, 5, 8), Vector3(3, 6, 9)))",
+            "Matrix3x3(Vector3(1, 4, 7), Vector3(2, 5, 8), Vector3(3, 6, 9))"
+            ),
+            TestCase(
+            "Matrix3x3(Vector3(1, 4, 7), Vector3(2, 5, 8), Vector3(3, 6, 9))" +
+            ".mul(Matrix3x3(Vector3(9, 6, 3), Vector3(8, 5, 2), Vector3(7, 4, 1)))",
+            "Matrix3x3(Vector3(30, 84, 138), Vector3(24, 69, 114), Vector3(18, 54, 90))"
+            ),
+        ]
+        public void CheckMul(string expression, string expected) =>
+            AssertApproxEqual(ValidatedCompilerInput, expected, expression);
+
+    }
+}
