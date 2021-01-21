0236cffa53ad79b55390cc3cf457da00bca9b14b
Craig Jeffrey <CraigJeffrey3@gmail.com>
2021-01-21 12:02:18 +0000

    Element.NET: Update formatting

--- Element.NET/!2-ValueObjectModel/Intrinsics/Functions/List.cs
+++ Element.NET/!2-ValueObjectModel/Intrinsics/Functions/List.cs
@@ -8,7 +8,7 @@ namespace Element.AST
     {
         private List()
         {
-            Identifier = new Identifier( "list" );
+            Identifier = new Identifier("list");
         }
 
         public static List Instance { get; } = new List();
@@ -16,17 +16,17 @@ namespace Element.AST
         public bool IsVariadic => true;
 
         public override Result<IValue> Call(IReadOnlyList<IValue> arguments, Context context) =>
-            context.RootScope.Lookup( ListStruct.Instance.Identifier, context )
-                   .Accumulate( () => context.RootScope.Lookup( NumStruct.Instance.Identifier, context ) )
-                   .Bind( t =>
+            context.RootScope.Lookup(ListStruct.Instance.Identifier, context)
+                   .Accumulate(() => context.RootScope.Lookup(NumStruct.Instance.Identifier, context))
+                   .Bind(t =>
                     {
                         var (listStruct, numStruct) = t;
-                        return listStruct.Call( new IValue[]
+                        return listStruct.Call(new IValue[]
                         {
-                            new ListIndexer( arguments, new[] {new ResolvedPort( numStruct )}, AnyConstraint.Instance ),
-                            new Constant( arguments.Count )
-                        }, context );
-                    } );
+                            new ListIndexer(arguments, new[] {new ResolvedPort(numStruct)}, AnyConstraint.Instance),
+                            new Constant(arguments.Count)
+                        }, context);
+                    });
 
         private class ListIndexer : Function
         {
@@ -43,21 +43,21 @@ namespace Element.AST
             public override IValue ReturnConstraint { get; }
 
             protected override Result<IValue> ResolveCall(IReadOnlyList<IValue> arguments, Context context) =>
-                arguments[0].InnerIs( out Instruction index )
-                    ? HomogenousListElement.Create( index, _elements, context )
-                    : context.Trace( EleMessageCode.ConstraintNotSatisfied, "List Index must be a Num" );
+                arguments[0].InnerIs(out Instruction index)
+                    ? HomogenousListElement.Create(index, _elements, context)
+                    : context.Trace(EleMessageCode.ConstraintNotSatisfied, "List Index must be a Num");
         }
 
         private class HomogenousListElement : IValue
         {
             public static Result<IValue> Create(Instruction index, IReadOnlyList<IValue> elements, Context context) =>
-                elements[0].InstanceType( context )
-                           .Bind( listElementInstanceType => elements.VerifyValuesAreAllOfInstanceType(
+                elements[0].InstanceType(context)
+                           .Bind(listElementInstanceType => elements.VerifyValuesAreAllOfInstanceType(
                                 listElementInstanceType,
-                                () => index.CompileTimeIndex( 0, elements.Count, context )
-                                           .Branch( compileConstantIndex => compileConstantIndex < elements.Count
-                                                    ? new Result<IValue>( elements[compileConstantIndex] )
-                                                    : context.Trace( EleMessageCode.ArgumentOutOfRange, $"Index {compileConstantIndex} out of range - list has {elements.Count} items" ),
+                                () => index.CompileTimeIndex(0, elements.Count, context)
+                                           .Branch(compileConstantIndex => compileConstantIndex < elements.Count
+                                                    ? new Result<IValue>(elements[compileConstantIndex])
+                                                    : context.Trace(EleMessageCode.ArgumentOutOfRange, $"Index {compileConstantIndex} out of range - list has {elements.Count} items"),
                                                 () =>
                                                 {
                                                     var operands = new Instruction[elements.Count];
@@ -65,12 +65,12 @@ namespace Element.AST
                                                     for (var i = 0; i < elements.Count; i++)
                                                     {
                                                         // If any elements are not instructions then we need to 
-                                                        if (elements[i].InnerIs( out Instruction instruction )) operands[i] = instruction;
-                                                        else return new Result<IValue>( new HomogenousListElement( index, elements ) );
+                                                        if (elements[i].InnerIs(out Instruction instruction)) operands[i] = instruction;
+                                                        else return new Result<IValue>(new HomogenousListElement(index, elements));
                                                     }
 
-                                                    return Switch.CreateAndOptimize( index, operands, context ).Cast<IValue>();
-                                                } ), context ) );
+                                                    return Switch.CreateAndOptimize(index, operands, context).Cast<IValue>();
+                                                }), context));
 
             private HomogenousListElement(Instruction index, IReadOnlyList<IValue> elements)
             {
@@ -80,23 +80,23 @@ namespace Element.AST
 
             private readonly Instruction _index;
             private readonly IReadOnlyList<IValue> _elements;
-            public string TypeOf => $"HomogenousListElement({_index})[{string.Join( ", ", _elements.Select( e => e.TypeOf ) )}]";
-            public string SummaryString => $"HomogenousListElement({_index})[{string.Join( ", ", _elements )}]";
+            public string TypeOf => $"HomogenousListElement({_index})[{string.Join(", ", _elements.Select(e => e.TypeOf))}]";
+            public string SummaryString => $"HomogenousListElement({_index})[{string.Join(", ", _elements)}]";
 
             private Result<IValue> ApplyToAllElements(Func<IValue, Result<IValue>> func, Context context)
-                => _elements.Select( func )
-                            .BindEnumerable( elements => Create( _index, elements.ToArray(), context ) );
+                => _elements.Select(func)
+                            .BindEnumerable(elements => Create(_index, elements.ToArray(), context));
 
             public IReadOnlyList<ResolvedPort> InputPorts => _elements[0].InputPorts;
             public IValue ReturnConstraint => _elements[0].ReturnConstraint;
             public IReadOnlyList<Identifier> Members => _elements[0].Members;
-            public Result<IValue> Index(Identifier id, Context context) => ApplyToAllElements( e => e.Index( id, context ), context );
-            public Result<IValue> Call(IReadOnlyList<IValue> arguments, Context context) => ApplyToAllElements( e => e.Call( arguments.ToArray(), context ), context );
-            public Result<bool> MatchesConstraint(IValue value, Context context) => context.Trace( EleMessageCode.NotCompileConstant, "ListElement index is unknown at compile time - it cannot be used as a constraint" );
-            public Result<IValue> DefaultValue(Context context) => ApplyToAllElements( e => e.DefaultValue( context ), context );
-            public void Serialize(ResultBuilder<List<Instruction>> resultBuilder, Context context) => context.Trace( EleMessageCode.NotCompileConstant, "ListElement index is unknown at compile time - it cannot be serialized" );
-            public Result<IValue> Deserialize(Func<Instruction> nextValue, Context context) => context.Trace( EleMessageCode.NotCompileConstant, "ListElement index is unknown at compile time - it cannot be used to deserialize" );
-            public Result<IValue> InstanceType(Context context) => _elements[0].InstanceType( context );
+            public Result<IValue> Index(Identifier id, Context context) => ApplyToAllElements(e => e.Index(id, context), context);
+            public Result<IValue> Call(IReadOnlyList<IValue> arguments, Context context) => ApplyToAllElements(e => e.Call(arguments.ToArray(), context), context);
+            public Result<bool> MatchesConstraint(IValue value, Context context) => context.Trace(EleMessageCode.NotCompileConstant, "ListElement index is unknown at compile time - it cannot be used as a constraint");
+            public Result<IValue> DefaultValue(Context context) => ApplyToAllElements(e => e.DefaultValue(context), context);
+            public void Serialize(ResultBuilder<List<Instruction>> resultBuilder, Context context) => context.Trace(EleMessageCode.NotCompileConstant, "ListElement index is unknown at compile time - it cannot be serialized");
+            public Result<IValue> Deserialize(Func<Instruction> nextValue, Context context) => context.Trace(EleMessageCode.NotCompileConstant, "ListElement index is unknown at compile time - it cannot be used to deserialize");
+            public Result<IValue> InstanceType(Context context) => _elements[0].InstanceType(context);
             public bool IsIntrinsicOfType<TIntrinsicImplementation>() where TIntrinsicImplementation : IIntrinsicImplementation => false;
             public bool IsSpecificIntrinsic(IIntrinsicImplementation intrinsic) => false;
         }
--- Element.NET/!2-ValueObjectModel/Values/StructuralTuple.cs
+++ Element.NET/!2-ValueObjectModel/Values/StructuralTuple.cs
@@ -11,20 +11,20 @@ namespace Element.AST
         private StructuralTuple(IReadOnlyList<(Identifier, IValue, IValue)> fields) => _fields = fields;
 
         public static Result<IValue> CreateInstance(ResolvedBlock block, Context context) =>
-            block.MemberValues( context )
-                 .Bind( members =>
+            block.MemberValues(context)
+                 .Bind(members =>
                   {
-                      var orderedMembers = members.OrderBy( m => m.Identifier.String ).ToArray();
+                      var orderedMembers = members.OrderBy(m => m.Identifier.String).ToArray();
 
                       // Replace argument block to ensure members are ordered by identifier
                       // NOTE: About member ordering - If we didn't order members then users could declare multiple structural tuples of the same "type" with different order and break serialization/deserialization.
-                      block = new ResolvedBlock( orderedMembers, null );
-                      return orderedMembers.Select( member => member.Value
-                                                                    .InstanceType( context )
-                                                                    .Map( type => (member.Identifier, member.Value, Type: type) ) )
+                      block = new ResolvedBlock(orderedMembers, null);
+                      return orderedMembers.Select(member => member.Value
+                                                                   .InstanceType(context)
+                                                                   .Map(type => (member.Identifier, member.Value, Type: type)))
                                            .ToResultArray();
-                  } )
-                 .Bind( members =>
+                  })
+                 .Bind(members =>
                   {
                       bool IsTupleOfCorrectType(StructuralTuple otherTuple)
                       {
@@ -37,25 +37,25 @@ namespace Element.AST
                           {
                               var (identifier, _, type) = members[index];
                               var (otherIdentifier, _, otherType) = otherTuple._fields[index];
-                              result |= identifier.Equals( otherIdentifier ) && type.Equals( otherType );
+                              result |= identifier.Equals(otherIdentifier) && type.Equals(otherType);
                           }
 
                           return result;
                       }
 
                       // Find the tuple type in context's known structural tuples or generate and add a new one
-                      if (!(context.StructuralTuples.FirstOrDefault( IsTupleOfCorrectType ) is { } tupleType))
+                      if (!(context.StructuralTuples.FirstOrDefault(IsTupleOfCorrectType) is { } tupleType))
                       {
-                          tupleType = new StructuralTuple( members );
-                          context.StructuralTuples.Add( tupleType );
+                          tupleType = new StructuralTuple(members);
+                          context.StructuralTuples.Add(tupleType);
                       }
 
-                      return new Result<IValue>( new StructuralTupleInstance( block, tupleType ) );
-                  } );
+                      return new Result<IValue>(new StructuralTupleInstance(block, tupleType));
+                  });
 
-        public override string SummaryString => $"Tuple<{string.Join( ", ", _fields.Select( f => f.Type ) )}>";
+        public override string SummaryString => $"Tuple<{string.Join(", ", _fields.Select(f => f.Type))}>";
 
-        public override Result<bool> MatchesConstraint(IValue value, Context context) => value.IsInstanceOfType( this, context );
+        public override Result<bool> MatchesConstraint(IValue value, Context context) => value.IsInstanceOfType(this, context);
 
         private sealed class StructuralTupleInstance : Value
         {
@@ -69,23 +69,23 @@ namespace Element.AST
                 _resolvedBlock = block;
             }
 
-            public override string TypeOf => $"Tuple<{string.Join( ", ", TupleType._fields.Select( f => f.Type ) )}>";
+            public override string TypeOf => $"Tuple<{string.Join(", ", TupleType._fields.Select(f => f.Type))}>";
             public override Result<IValue> InstanceType(Context context) => TupleType;
-            public override Result<IValue> Index(Identifier id, Context context) => _resolvedBlock.Index( id, context );
+            public override Result<IValue> Index(Identifier id, Context context) => _resolvedBlock.Index(id, context);
             public override IReadOnlyList<Identifier> Members => _resolvedBlock.Members;
 
-            public override void Serialize(ResultBuilder<List<Instruction>> resultBuilder, Context context) => _resolvedBlock.Serialize( resultBuilder, context );
+            public override void Serialize(ResultBuilder<List<Instruction>> resultBuilder, Context context) => _resolvedBlock.Serialize(resultBuilder, context);
 
             public override Result<IValue> Deserialize(Func<Instruction> nextValue, Context context) =>
-                _resolvedBlock.DeserializeMembers( nextValue, context )
-                              .Map( deserializedFields =>
+                _resolvedBlock.DeserializeMembers(nextValue, context)
+                              .Map(deserializedFields =>
                                {
-                                   var newBlock = new ResolvedBlock( deserializedFields
-                                                                    .Zip( _resolvedBlock.Members,
-                                                                         (value, identifier) => (identifier, value) )
-                                                                    .ToArray(), null );
-                                   return new StructuralTupleInstance( newBlock, TupleType );
-                               } ).Cast<IValue>();
+                                   var newBlock = new ResolvedBlock(deserializedFields
+                                                                   .Zip(_resolvedBlock.Members,
+                                                                        (value, identifier) => (identifier, value))
+                                                                   .ToArray(), null);
+                                   return new StructuralTupleInstance(newBlock, TupleType);
+                               }).Cast<IValue>();
         }
     }
 }
