e71aaa4eb3531a909d11e9f8acd334d870a7c388
Andy Martin <andy.martin@ultraleap.com>
2021-05-26 11:52:47 +0100

    Laboratory: fixed circle tests in StandardPaths to match updated circle path behaviour

--- Laboratory/Tests/L4-Standard-Library/StandardPaths.cs
+++ Laboratory/Tests/L4-Standard-Library/StandardPaths.cs
@@ -6,19 +6,19 @@ namespace Laboratory.Tests.L4.StandardLibrary
     {
         public static (string circleConstructorArgs, string evaluationArgs, string expected)[] CircleArgList =
         {
-            ("1", "0", "Vector3(0, 1, 0)"),
+            ("1", "0", "Vector3(1, 0, 0)"),
             ("1", "0.125", "Vector3(2.pow(-0.5), 2.pow(-0.5), 0)"),
-            ("1", "0.25", "Vector3(1, 0, 0)"),
-            ("1", "0.375", "Vector3(2.pow(-0.5), 2.pow(-0.5).mul(-1), 0)"),
-            ("1", "0.5", "Vector3(0, -1, 0)"),
+            ("1", "0.25", "Vector3(0, 1, 0)"),
+            ("1", "0.375", "Vector3(2.pow(-0.5).mul(-1), 2.pow(-0.5), 0)"),
+            ("1", "0.5", "Vector3(-1, 0, 0)"),
             ("1", "0.625", "Vector3(2.pow(-0.5).mul(-1), 2.pow(-0.5).mul(-1), 0)"),
-            ("1", "0.75", "Vector3(-1, 0, 0)"),
-            ("1", "0.875", "Vector3(2.pow(-0.5).mul(-1), 2.pow(-0.5), 0)"),
-            ("1", "1", "Vector3(0, 1, 0)"),
-            ("2", "0", "Vector3(0, 2, 0)"),
-            ("2", "0.25", "Vector3(2, 0, 0)"),
-            ("2", "0.5", "Vector3(0, -2, 0)"),
-            ("2", "0.75", "Vector3(-2, 0, 0)"),
+            ("1", "0.75", "Vector3(0, -1, 0)"),
+            ("1", "0.875", "Vector3(2.pow(-0.5), 2.pow(-0.5).mul(-1), 0)"),
+            ("1", "1", "Vector3(1, 0, 0)"),
+            ("2", "0", "Vector3(2, 0, 0)"),
+            ("2", "0.25", "Vector3(0, 2, 0)"),
+            ("2", "0.5", "Vector3(-2, 0, 0)"),
+            ("2", "0.75", "Vector3(0, -2, 0)"),
         };
         [Test]
         public void Circle([ValueSource(nameof(CircleArgList))]
