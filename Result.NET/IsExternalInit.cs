23efe8a5906f563be51c122e8d303f559f9af833
Craig Jeffrey <CraigJeffrey3@gmail.com>
2021-07-16 16:12:47 +0100

    Element.NET: Remove init-only properties (including record types) as they break in Unity

--- Result.NET/IsExternalInit.cs
+++
@@ -1,6 +0,0 @@
-namespace System.Runtime.CompilerServices
-{
-    // Fix to allow record type primary constructor to work with older than .NET 5.0
-    // See https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
-    internal static class IsExternalInit {}
-}
